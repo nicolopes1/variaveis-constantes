5dadda9a7a26a8edbbe4977d831795b634d23fe1
nicollas <nicollaslopesjf@gmail.com>
2018-08-30 20:45:45 -0300

    file

---
+++ aula-8-variaveis-constantes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_8_variaveis_constantes
+{
+    class Program
+    {
+        public const int SAIDA_PROGRAMA = 1;
+                
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Digite " + SAIDA_PROGRAMA + " para sair do programa ...");
+                int valor = int.Parse(Console.ReadLine());
+                if (SAIDA_PROGRAMA == valor)
+                    break;
+            }
+        }
+    }
+}
+
+/*
+
+Console.WriteLine("Digite o valor"); > c#
+int valor = Console.ReadLine();
+
+valor = input("Digite o valor"); > python
+
+printf ("Digite o valor"); > c
+scanf("%d", valor);
+
+*/
